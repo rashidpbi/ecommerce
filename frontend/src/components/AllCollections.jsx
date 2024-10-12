fa26f8967580c1318f7b62596a17842842a0f022
rashidpbi <rashidpbi786@gmail.com>
2024-10-12 11:27:25 +0530

    simple about page

--- frontend/src/components/AllCollections.jsx
+++ frontend/src/components/AllCollections.jsx
@@ -1,72 +1,150 @@
-import Title from './Title'
-import { useContext, useEffect, useState } from 'react'
-import ProductItem from './ProductItem'
-import { ShopContext } from '../context/ShopContext'
+import Title from "./Title";
+import { useContext, useEffect, useState } from "react";
+import ProductItem from "./ProductItem";
+import { ShopContext } from "../context/ShopContext";
 
 const AllCollections = () => {
+  const { products } = useContext(ShopContext);
+  const [collection, setCollection] = useState(products);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [displayProducts, setDisplayProducts] = useState(collection);
+  const [value, setValue] = useState("rel");
+  const [categories, setCategories] = useState({
+    men: false,
+    women: false,
+    kids: false,
+  });
+  // const [type,setType]= usestate(null)
+  const handleSortChange = (event) => {
+    setValue(event.target.value);
+  };
+  // const handleCheckboxChange = (event) => {
+  //   const { name, checked } = event.target;
+  //   setCategories((prevCategories) => ({
+  //     ...prevCategories,
+  //     [name]: checked,
+  //   }));
+  // };
+  // const applyFilters = () => {
+  //   const selectedCategories = Object.keys(categories).filter(
+  //     (category) => categories[category]
+  //   );
+  //   console.log('Selected Categories:', selectedCategories);
+  // };
 
-    const {products} = useContext(ShopContext)
-    const [collection,setCollection] = useState([])
-    const [value, setValue] = useState('rel');
-    // const [categories,setCategories]= useState(null)
-    // const [type,setType]= usestate(null)
-    const handleSortChange = (event) => {
+  useEffect(() => {
+    // let filteredProducts = collection
+    // if(categories){
+    // // console.log(categories[0])
+    // if(categories.men==false && categories.women==false && categories.kids==true){
+    //    filteredProducts = filteredProducts.filter((product)=>(product.category =='Kids'));
+    // }else if(categories.men==false && categories.women==true && categories.kids==false){
+    //   filteredProducts = filteredProducts.filter((product)=>(product.category =='Women'));
 
-        setValue(event.target.value);
-     
-      };
+    // }
+    // else if(categories.men==false && categories.women==true && categories.kids==true){
+    //   filteredProducts = filteredProducts.filter((product)=>(product.category =='Kids'||product.category =='Women'));
 
+    // }
+    // else if(categories.men==true && categories.women==false && categories.kids==false){
+    //   filteredProducts = filteredProducts.filter((product)=>(product.category =='Men'));
 
-useEffect(()=>{
-const collectionProducts = products.slice(0,15)
+    // }
+    // else if(categories.men==true && categories.women==true && categories.kids==false){
+    //   filteredProducts = filteredProducts.filter((product)=>(product.category =='Men'||product.category =='Women'));
 
-const filteredProducts = collectionProducts;
-if(value=='rel'){
-const sortedProducts = filteredProducts.sort((a, b) => parseFloat(a.date) - parseFloat(b.date))
-    setCollection(sortedProducts)
-}else if(value=='low to h'){
-    const sortedProducts = filteredProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
+    // }
+    // else {
+    //   filteredProducts = filteredProducts.filter((product)=>(product.category =='Men'||product.category =='Kids'));
 
-setCollection(sortedProducts)
-}else{
-    const sortedProducts = filteredProducts.sort((a, b) => parseFloat(b.price) - parseFloat(a.price))
+    // }}
 
-setCollection(sortedProducts)
-}
-},[value])
+    if (value == "rel") {
+      const sortedProducts = collection.sort(
+        (a, b) => parseFloat(a.date) - parseFloat(b.date)
+      );
+      setCollection(sortedProducts);
+    } else if (value == "low to h") {
+      const sortedProducts = collection.sort(
+        (a, b) => parseFloat(a.price) - parseFloat(b.price)
+      );
+
+      setCollection(sortedProducts);
+    } else {
+      const sortedProducts = collection.sort(
+        (a, b) => parseFloat(b.price) - parseFloat(a.price)
+      );
+
+      setCollection(sortedProducts);
+    }
+
+    setDisplayProducts(collection);
+  }, [value, displayProducts]);
   return (
     <div>
+      {/* <div>
+        <h3>Categories</h3>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              name="men"
+              checked={categories.men}
+              onChange={handleCheckboxChange}
+            />
+            Men
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              name="women"
+              checked={categories.women}
+              onChange={handleCheckboxChange}
+            />
+            Women
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              name="kids"
+              checked={categories.kids}
+              onChange={handleCheckboxChange}
+            />
+            Kids
+          </label>
+        </div>
 
-filer
-
+        <button onClick={applyFilters}>Apply Filters</button>
+      </div> */}
       <div className="flex justify-between ">
         <Title text1={"ALL"} text2={"COLLECTIONS"} />
         <div>
+          <select value={value} onChange={handleSortChange}>
+            <option value="rel">rel</option>
 
-<select value={value} onChange={handleSortChange}>
+            <option value="low to h">low to h</option>
 
-  <option value="rel">rel</option>
-
-  <option value="low to h">low to h</option>
-
-  <option value="h to l">h to l</option>
-
-</select>
-
-
-</div>
-
+            <option value="h to l">h to l</option>
+          </select>
+        </div>
       </div>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-
-          {
-              collection.map((item,index)=>(
-          <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-              ))
-            }
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {displayProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllCollections
+export default AllCollections;
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
-
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="text-center py-8">
+      <div className=" text-3xl">
+        <Title text1={"ABOUT"} text2={"US"} />{" "}
+      </div>
+      <div className="flex flex-col sm:flex-row gap-10 text-center justify-center ">
+        <img
+          src={assets.about_img}
+          className="mb-10 w-full md:max-w-[450px] "
+          alt=""
+        />
+        <div className="text-base text-gray-500 flex flex-col gap-8 w-full md:max-w-[450px]">
+          <p>
+            Forever was born out of a passion for innovation and a desire to
+            revolutionize the way people shop online. Our journey began with a
+            simple idea: to provide a platform where customers can easily
+            discover, explore, and purchase a wide range of products from the
+            comfort of their homes.
+          </p>
+          <p>
+            Since our inception, we've worked tirelessly to curate a diverse
+            selection of high-quality products that cater to every taste and
+            preference. From fashion and beauty to electronics and home
+            essentials, we offer an extensive collection sourced from trusted
+            brands and suppliers.
+          </p>
+          <p className="text-gray-600 font-semibold">Our Mission</p>
+          <p>
+            Our mission at Forever is to empower customers with choice,
+            convenience, and confidence. We're dedicated to providing a seamless
+            shopping experience that exceeds expectations, from browsing and
+            ordering to delivery and beyond.
+          </p>
+        </div>
+      </div>
+      <div className=" text-3xl">
+        <Title text1={"ABOUT"} text2={"US"} />{" "}
+      </div>
+      <div className="flex flex-col md:flex-row  ">
+        <div className="border">
+          <p>Quality Assurance:</p>
+          <p>
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <hr />
 
-export default About
+        <div className="border">
+          <p>Convenience:</p>
+          <p>
+            With our user-friendly interface and hassle-free ordering process,
+            shopping has never been easier.
+          </p>
+        </div>
+        <div className="border">
+          <p>Exceptional Customer Service:</p>
+          <p>
+            Our team of dedicated professionals is here to assist you the way,
+            ensuring your satisfaction is our top priority.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
