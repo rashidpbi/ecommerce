bf9939bffefdfb22e5a413039566f2fef8eef345
rashidpbi <rashidpbi786@gmail.com>
2024-10-17 14:39:52 +0530

    products added to db manually

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login";
 import { ToastContainer } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const currency = '$'
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
