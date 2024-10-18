a18a0067c15a0435a7979885a7124d115430e382
rashidpbi <rashidpbi786@gmail.com>
2024-10-18 21:52:42 +0530

    place order functionality added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import BestSeller from '../components/BestSeller'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection'
 import NewsLetterBox from '../components/NewsLetterBox'
 import OurPolicy from '../components/OurPolicy'
 
@@ -9,7 +8,6 @@ const Home = () => {
   return (
     <div>
       <Hero/>
-      <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
       <NewsLetterBox/>
