ea5dc787ebfb0943184e7f32710ed5034806005f
rashidpbi <rashidpbi786@gmail.com>
2024-10-19 11:45:37 +0530

    connected frontend to backend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,9 +1,8 @@
-import { createContext, useEffect } from "react";
-import { products } from "../assets/assets";
-import { useState } from "react";
+
+import { createContext, useEffect,useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import { backendUrl } from "../../../admin/src/App";
+import axios from 'axios'
 
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
@@ -13,7 +12,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
-  // const [products,setProducts] = useState([])
+  const [products,setProducts] = useState([])
  const navigate = useNavigate()
   const addToCart = async (itemId, size) => {
     if (!size) {
@@ -84,12 +83,23 @@ setCartItems(cartData)
     const getProductsData = async()=>{
 
       try {
+        const response = await axios.get(backendUrl + '/api/product/list')
+        if (response.data.success ) {
+          setProducts(response.data.products)
+          
+        }else{
+          toast.error(response.data.message)
+        }
         
       } catch (error) {
+        console.log(error);
+        toast.error(error.message)
         
       }
     }
-
+useEffect(()=>{
+getProductsData()
+},[])
   const value = {
     products,
     currency,
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BestSeller from '../components/BestSeller'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
 import NewsLetterBox from '../components/NewsLetterBox'
 import OurPolicy from '../components/OurPolicy'
 
@@ -8,6 +9,8 @@ const Home = () => {
   return (
     <div>
       <Hero/>
+      <LatestCollection/>
+
       <BestSeller/>
       <OurPolicy/>
       <NewsLetterBox/>
