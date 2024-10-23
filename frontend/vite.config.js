6ff08d49de119c7d0241771992906675e8a6c484
rashidpbi <rashidpbi786@gmail.com>
2024-10-23 23:01:25 +0530

    change in admin vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  server: {
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      'react-router-dom': require.resolve('react-router-dom'),
+    },
+  },
+});
