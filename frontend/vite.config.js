00750e17abf65bf96532210f9d9359051b825904
rashidpbi <rashidpbi786@gmail.com>
2024-10-23 23:31:49 +0530

    change in admin vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +8,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'react-router-dom': require.resolve('react-router-dom'),
+      'react-router-dom': 'react-router-dom',  
     },
   },
 });
