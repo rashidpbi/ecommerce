a93bb08184e47dcc377b4e4cf13c97b647e91b41
rashidpbi <rashidpbi786@gmail.com>
2024-10-24 17:26:31 +0530

    removed new router dom import format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Orders from "./pages/Orders";
