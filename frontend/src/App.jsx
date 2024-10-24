f5f3532bf428df25b4c8209630ca12dd285fd345
rashidpbi <rashidpbi786@gmail.com>
2024-10-24 17:13:21 +0530

    changed import statement of router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Orders from "./pages/Orders";
